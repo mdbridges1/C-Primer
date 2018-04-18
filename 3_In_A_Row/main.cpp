9434e94e0ad6b4f9473df7ddf1097df78e508a9b
Michael Bridges <mike@michaelbridges.co.uk>
2018-04-18 17:09:27 +0100

    Generate and output random number
    Notice: Random number is the same every time
    cout << rand();
    
    Learn how to restrict to a range %100 (0-99)
    + 1 for 1-100

--- 3_In_A_Row/main.cpp
+++ 3_In_A_Row/main.cpp
@@ -5,10 +5,15 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+
+void DisplayGameRules();
+void GenerateRandomNumber();
 
 int main()
     {
-    std::cout << "Welcome to 3 In A Row\n";
+        DisplayGameRules();
+        GenerateRandomNumber();
         // Show play the rules for the game
         // Start displaying random numbers
         // If 3 in a row are odd, start timer- continue random numbers store 3 in a row values
@@ -18,3 +23,17 @@ int main()
         // Quit
     return 0;
     }
+void DisplayGameRules()
+{
+    std::cout   << "Welcome to 3 In A Row\n"
+    << "A series of random numbers will now be shown\n"
+    << "When you nseen 3 odd numbers in a row, press Enter\n"
+    << "Be quick, I'm timing you\n\n"
+    << "Press enter to start the game\n\n";;
+}
+
+void GenerateRandomNumber()
+{
+    std::cout << (rand() %100 ) << std::endl; // notice it is the same random number everytime!
+    
+}
