09c88b50b97039afe8bb9de0d1e584152b6df148
Michael Bridges <mike@michaelbridges.co.uk>
2018-04-18 16:15:07 +0100

    Initial Commit

---
+++ 3_In_A_Row/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3_In_A_Row
+//
+//  Created by Michael Bridges on 18/04/2018.
+//  Copyright © 2018 Michael Bridges. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
