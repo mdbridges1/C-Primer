79236984b21d60bea5e1bbf2c477fde7746ad356
Michael Bridges <mike@michaelbridges.co.uk>
2018-04-18 16:32:59 +0100

    Rough game layout in pusedo code

--- 3_In_A_Row/main.cpp
+++ 3_In_A_Row/main.cpp
@@ -9,5 +9,12 @@
 int main()
     {
     std::cout << "Welcome to 3 In A Row\n";
+        // Show play the rules for the game
+        // Start displaying random numbers
+        // If 3 in a row are odd, start timer- continue random numbers store 3 in a row values
+        // When user presses enter - display win or lose
+        // Lose Screen - Show reason for losing - Play Again?
+        // Win Screen - Play again
+        // Quit
     return 0;
     }
