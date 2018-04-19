6120befdcb5fece766a550e802a6b68670bb15f6
Michael Bridges <mike@michaelbridges.co.uk>
2018-04-19 12:21:20 +0100

    Only Unique Number Passed Through
    Now only unique numbers trigger game end (timerstart)

--- 3_In_A_Row/main.cpp
+++ 3_In_A_Row/main.cpp
@@ -16,42 +16,28 @@
 #endif
 
 void DisplayGameRules();
+void PlayGame();
 void SetRandomSeed();
-void DisplayRandomNumber();
+void PopulateArray();
 void SortArrayElements();
 void AreArrayElementsUnique();
 void DisplayOddNumberArray();
 void IsOddNumber();
 
-//Global variables are bad practice- return from functions?
+//Global variables are bad practice - return from functions?
 const int arraySize = 3;
 int oddNumberArray[arraySize]; // consider list or un ordered set
 int countOddNumbers = 0;
 int randomNumber;
 
-int timeBetweenNumbers = 10; //time in ms
+int timeBetweenNumbers = 100; //time in ms
 
 int main()
     {
         DisplayGameRules();
         SetRandomSeed();
-        do
-        {
-            randomNumber =(rand() %100 + 1);
-            std::cout << randomNumber << std::endl;
-            DisplayRandomNumber();
-        }
-        while(countOddNumbers < arraySize);
-        SortArrayElements();
-        AreArrayElementsUnique();
-        DisplayOddNumberArray();
+        PlayGame();
         
-        
-        // TODO odd needs to be unique
-        // When user presses enter - display win or lose
-        // Lose Screen - Show reason for losing - Play Again?
-        // Win Screen - Play again
-        // Quit
     return 0;
     }
 
@@ -59,11 +45,30 @@ void DisplayGameRules()
 {
     std::cout   << "Welcome to 3 In A Row\n"
     << "A series of random numbers will now be shown\n"
-    << "When you nseen 3 odd numbers in a row, press Enter\n"
+    << "When you see 3 UNIQUE odd numbers in a row, press Enter\n"
     << "Be quick, I'm timing you\n\n"
     << "Press ENTER to start the game\n\n";;
-    std::cin.ignore()
-    ;
+    std::cin.ignore();
+}
+
+void PlayGame()
+{
+    do
+    {
+        randomNumber =(rand() %10 + 1);
+        std::cout << randomNumber << std::endl;
+        PopulateArray();
+    }
+    while(countOddNumbers < arraySize);
+    SortArrayElements();
+    AreArrayElementsUnique();
+    DisplayOddNumberArray();
+    
+    // TODO odd needs to be unique
+    // When user presses enter - display win or lose
+    // Lose Screen - Show reason for losing - Play Again?
+    // Win Screen - Play again
+    // Quit
 }
 
 void SetRandomSeed()
@@ -73,7 +78,7 @@ void SetRandomSeed()
     return;
 }
 
-void DisplayRandomNumber() {
+void PopulateArray() {
     
     // is the number odd?
     
@@ -97,6 +102,29 @@ void SortArrayElements()
 
 void AreArrayElementsUnique()
 {
+    int origVal = 0, compVal = 0, a, b;
+    
+    for(a = 0 ; a < arraySize ; a++)//starting point in array
+    {
+        origVal = oddNumberArray[a];
+        
+        for (b = a+ 1 ; b < arraySize ; b++)
+        {
+            if(origVal == oddNumberArray[b])
+            {
+                compVal = 1;
+            }
+        }
+    }
+    if (compVal == 1)
+    {
+        std::cout << "NOT UNIQUE";
+        PlayGame();
+    }
+    else
+    {
+        std::cout << "UNIQUE" ;
+    }
     //step through array and see if any two consecutive numbers are the same
     //start with array element n, compare to n++
     //increase n but one and check again.
